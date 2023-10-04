70491a7eda0f363d25bec53749ac3b09107ab47b
Enes Yüksel <enesyuksel9903@gmail.com>
2023-10-04 10:45:22 +0300

    Usage for server side

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,22 @@
 import Link from "next/link";
+import { getLocale } from "@/utils/getLocale";
+import { useTranslation } from "@/app/i18n";
 
-export default function Home() {
+export default async function Home() {
+    const locale = await getLocale();
+    const { t } = await useTranslation(locale);
+
     return (
         <div className="text-center">
-            <h1 className="font-bold text-4xl mb-4">Welcome!</h1>
-            <p className="font-thin mb-2">
-                In todays rapidly evolving technological landscape, artificial
-                intelligence (AI) is playing an increasingly pivotal role in
-                reshaping the way we communicate. One remarkable development in
-                this realm is the emergence of ChatGPT, a cutting-edge AI
-                language model. Developed by OpenAI, ChatGPT is built upon the
-                GPT-3.5 architecture, a sophisticated neural network that has
-                been trained on vast amounts of text data. ChatGPT holds immense
-                promise for revolutionizing the way we interact with technology.
-                What sets it apart is its ability to understand and generate
-                human-like text responses, making it an excellent tool for
-                natural language processing tasks. Whether you want to ask
-                questions, seek information, or engage in casual conversation,
-                ChatGPT is readily accessible through web interfaces or
-                integrated into various applications.
-            </p>
-            <p className="font-thin">
-                One of the most significant advantages of ChatGPT is its
-                user-friendliness. It simplifies complex interactions with AI,
-                eliminating the need for specialized technical knowledge.
-                Furthermore, it can assist users across a wide spectrum of
-                domains, from aiding in learning processes to providing creative
-                writing prompts and even assisting in programming tasks. Its
-                vast knowledge base allows it to be a versatile companion.
-                However, it is essential to recognize the limitations of
-                ChatGPT. As a text-based AI, it lacks the ability to process
-                images, audio, or video. Additionally, like all AI systems, it
-                may occasionally provide inaccurate or misleading information,
-                raising ethical and moral concerns. Users should exercise
-                caution and verify information when relying on ChatGPT. In
-                conclusion, ChatGPT represents a significant advancement in
-                AI-driven communication. It empowers individuals to engage with
-                technology in more intuitive and human-like ways, but it is not
-                without its caveats. As AI continues to evolve, ChatGPT and
-                similar models will likely play an increasingly prominent role
-                in shaping the future of communication.
-            </p>
+            <h1 className="font-bold text-4xl mb-4">{t("title")}</h1>
+            <p className="font-thin mb-2">{t("paragraph1")}</p>
+            <p className="font-thin">{t("paragraph2")}</p>
 
             <Link
                 className="block bg-slate-100 text-black mt-6 w-[200px] m-auto py-1 rounded-sm font-bold hover:bg-slate-300"
                 href="/explore"
             >
-                Explore
+                {t("explore")}
             </Link>
         </div>
     );

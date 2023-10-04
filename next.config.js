3db28c808646613824dbc16544e9fb2ccef05502
Enes Yüksel <enesyuksel9903@gmail.com>
2023-10-04 10:27:54 +0300

    next.config.js rewrites

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const defaultLocale = "tr";
+const locales = ["tr", "en"];
 
-module.exports = nextConfig
+const rewrites = [];
+
+locales.forEach((locale) => {
+    if (locale === defaultLocale) {
+        return;
+    }
+
+    rewrites.push({
+        source: `/${locale}`,
+        destination: `/`,
+    });
+
+    rewrites.push({
+        source: `/${locale}/:path*`,
+        destination: `/:path*`,
+    });
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    async rewrites() {
+        return rewrites;
+    },
+};
+
+module.exports = nextConfig;

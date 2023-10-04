ab10ddc1bb1f2d866038a990b233ca07fee380ad
Enes Yüksel <enesyuksel9903@gmail.com>
2023-10-04 10:41:34 +0300

    dummy design

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body className="p-10 bg-slate-900 text-white">{children}</body>
         </html>
     );
 }
